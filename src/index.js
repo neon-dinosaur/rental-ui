ea72072a851c6a25875595c79afba08869fabebb
Phil <phil.boothe@gmail.com>
2017-10-05 21:37:49 -0400

    Add react_router to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { BrowserRouter, Match } from 'react-router';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+
 registerServiceWorker();
+
+const Root = () => {
+    return (
+    <BrowserRouter>
+        <div>
+          <Match exactly pattern="/" component={App} />
+        </div>
+      </BrowserRouter>
+    )
+}
+
+render(<Root />, document.getElementById('root'));
