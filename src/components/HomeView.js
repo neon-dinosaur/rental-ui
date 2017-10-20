d43f38497d367ec1a9abf8a13da0eebaadcaa7d0
Phil Boothe <phil.boothe@gmail.com>
2017-10-19 23:45:28 -0400

    Support custom CSS and start styling Homeview (#11)

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -5,9 +5,9 @@ const HomeView = () => (
         <div className="columns">
             <div className="column col-4 hide-sm">
             </div>
-            <div className="column col-6 col-mx-auto">
-                <h1 className="homeview-title">Welcome to Sandride Beach</h1>
-                <p className="homeview-content">
+            <div className="homeview-content column col-6 col-mx-auto">
+                <h1>Welcome to Sandride Beach</h1>
+                <p>
                     Aenean sed lacinia nisi, convallis tincidunt massa. Morbi laoreet congue dolor, et gravida ex ultricies eget.
                     Fusce sodales leo eu dui dictum tincidunt. Sed malesuada, purus eget mollis viverra, felis eros euismod velit, sed finibus nibh quam nec sapien.
                     Phasellus non turpis ac felis fringilla ullamcorper. Fusce eget auctor quam. Nullam faucibus nunc tellus, id pretium erat iaculis vitae.
