4b8fbab382b230ead3f0d444bee9726fe7f3edcf
Phil <phil.boothe@gmail.com>
2017-11-01 21:02:35 -0400

    actual real style changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 const contactInfo = [
-    {value: "2017 Ocean Rentals Ltd", width: "col-3"},
-    {value: "577 Sandbridge Road, Virginia Beach, VA 23456", width: "col-4"},
-    {value: "757.721.6210", width: "col-2"},
-    {value: "SurfAndAdventure@gmail.com", width: "col-3"}
+    { value: "2017 Ocean Rentals Ltd", width: "col-3" },
+    { value: "577 Sandbridge Road, Virginia Beach, VA 23456", width: "col-4" },
+    { value: "757.721.6210", width: "col-2" },
+    { value: "SurfAndAdventure@gmail.com", width: "col-3" }
 ];
 
 const Footer = () => (
     <div className="footer">
-        <div className="columns col-oneline">
-            {
-                contactInfo.map(
-                    (content, index) => (<p key={index} className={`footer-content column ${content.width}`}>{content.value}</p>)
-                )
-            }
+        <div className="footer-content">
+            <div className="columns col-oneline">
+                {
+                    contactInfo.map(
+                        (content, index) => (<p key={index} className={`column ${content.width}`}>{content.value}</p>)
+                    )
+                }
+            </div>
         </div>
     </div>
 );
